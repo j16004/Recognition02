8925c4ad861857c90b6a31b93057202b73a03352
J16004 <J16004@10.206.1.108>
2018-05-24 12:56:36 +0900

    5月24日

--- src/recognition02/Recognition02_main.java
+++ src/recognition02/Recognition02_main.java
@@ -2,9 +2,12 @@ package recognition02;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
@@ -33,6 +36,36 @@ public class Recognition02_main {
 		System.out.println(result);
 
 
+		String s = String.valueOf(result);
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = null;
+
+		try {
+			node = mapper.readTree(s);
+
+			String class_0 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class").asText();
+			System.out.println("class_0 : " + class_0);
+
+			float class_0_s = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score").floatValue();
+			System.out.println("class_0_s : " + class_0_s);
+
+			String class_1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class").asText();
+			System.out.println("class_1 : " +class_1);
+
+			float class_1_s = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score").floatValue();
+			System.out.println("class_1_s : " + class_1_s);
+
+			String class_2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class").asText();
+			System.out.println("class_2 : " + class_2);
+
+			float class_2_s = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score").floatValue();
+			System.out.println("class_2_s : " + class_2_s);
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
 	}
+
 }
